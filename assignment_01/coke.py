03c72d14465f6c51170191b9834d3d46257fd39a
Danilo III Gonzales <dgonzales@edu.ulisboa.pt>
2025-10-23 05:41:01 +0800

    Updated solution to coke.py by moving the output of amount due inside the loop.

--- assignment_01/coke.py
+++ assignment_01/coke.py
@@ -11,17 +11,14 @@
 # 2. If the total amount of inserted coins is equal to or greater than 50 cents, the program displays
 #    the change owed and terminates.
 
-amount_due = 50  # Variable declaration and initialization for the amount due of one bottle of Coca-Cola in the vending machine.
-print(f"Amount due: {amount_due}")  # Displays the initial amount due, which is 50 cents.
+amount_due = 50  # Variable declaration and initialization for the amount due of one bottle of Coca-Cola in the vending machine
 
 while amount_due > 0:  # The while loop continues to prompt for coins while the amount due is greater than 0.
+    print(f"Amount due: {amount_due}")  # Displays the initial amount due, which is 50 cents.
     inserted_coin = int(input("Insert Coin: "))  # Prompts the user to insert a coin.
 
     if inserted_coin == 25 or inserted_coin == 10 or inserted_coin == 5:  # Checks if the inserted coin is an accepted denomination.
         amount_due -= inserted_coin  # Subtracts the value of the valid coin from the amount due.
-
-    if amount_due > 0:  # If there is still an amount due, display the remaining balance.
-        print(f"Amount due: {amount_due}")
 
 amount_due = abs(amount_due)  # Uses the abs() function to get the absolute value of the amount due for the change owed.
 print(f"Change Owed: {amount_due}")  # Displays the change owed once the amount due is 0 or less.
